1f75168b8d5503259889eb883e82900eb6fc46ee
AudreyJones <audrey.eliz.jones@gmail.com>
2019-04-24 13:28:33 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
@@ -19,7 +20,9 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+    
+    @post.update(params.require(:post).permit(:title,:description))
+    # binding.pry
     redirect_to post_path(@post)
   end
 
